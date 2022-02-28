0d3db3a890224257c488565c76089cece5077b12
Assad Isah <nottherealalanturing@gmail.com>
2022-02-28 16:55:07 +0100

    refactor: add a seperate navbar/menu for desktop and mobile

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,6 @@
 let hamburger = document.getElementById("nav-button");
 let menu = document.getElementById("menu");
+
 hamburger.addEventListener("click", (event) => {
   menu.classList.toggle("nav-mobile");
 });
