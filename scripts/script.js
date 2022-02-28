0714b837456c0126be8c0e82bc3f3a1ad3ca8fe9
Assad Isah <nottherealalanturing@gmail.com>
2022-02-28 23:01:08 +0100

    feat: add auto close to navigation buttons

--- scripts/script.js
+++ scripts/script.js
@@ -2,15 +2,26 @@ const hamburger = document.getElementById('nav-button');
 const menu = document.getElementById('menu');
 const navButton = document.getElementById('nav-button');
 const closebtn = document.getElementById('closebtn');
+const menuItems = Array.from(
+  document.getElementById('overlay-content').children
+);
 
-hamburger.addEventListener('click', (event) => {
+menuItems.forEach((li) => {
+  li.addEventListener('click', () => {
+    document.getElementById('myNav').style.width = '0%';
+    document.getElementById('body').style.overflow = 'auto';
+  });
+});
+hamburger.addEventListener('click', () => {
   menu.classList.toggle('nav-mobile');
 });
 
 closebtn.addEventListener('click', () => {
   document.getElementById('myNav').style.width = '0%';
+  document.getElementById('body').style.overflow = 'auto';
 });
 
 navButton.addEventListener('click', () => {
   document.getElementById('myNav').style.width = '100%';
+  document.getElementById('body').style.overflow = 'hidden';
 });
