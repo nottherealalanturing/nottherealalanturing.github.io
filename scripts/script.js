dc70534b32729c5617ce62a112fd3025aa3c2071
Assad Isah <nottherealalanturing@gmail.com>
2022-03-01 08:58:06 +0100

    feat: add mix-blend-mode to overlay background

--- scripts/script.js
+++ scripts/script.js
@@ -19,9 +19,13 @@ hamburger.addEventListener('click', () => {
 closebtn.addEventListener('click', () => {
   document.getElementById('myNav').style.width = '0%';
   document.getElementById('body').style.overflow = 'auto';
+  document.getElementById('headline').classList.remove('blur');
+  document.getElementById('container').classList.remove('blur');
 });
 
 navButton.addEventListener('click', () => {
   document.getElementById('myNav').style.width = '100%';
   document.getElementById('body').style.overflow = 'hidden';
+  document.getElementById('headline').classList.add('blur');
+  document.getElementById('container').classList.add('blur');
 });
