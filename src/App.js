c2e9d33cf505506b9d34031c1a1e24c56c5e23bb
Assad Isah <nottherealalanturing@gmail.com>
2022-07-01 21:10:21 +0100

    fix: fix from linters

--- src/App.js
+++ src/App.js
@@ -4,9 +4,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit
+          {' '}
+          <code>src/App.js</code>
+          {' '}
+          and save to reload.
         </p>
         <a
           className="App-link"
