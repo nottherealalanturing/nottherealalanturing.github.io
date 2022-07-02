6a9112c5399b3e3a3cdb04e9b4db50d436255fd9
Assad Isah <nottherealalanturing@gmail.com>
2022-07-02 01:21:39 +0100

    feat: add navbar and font - Add navbar for desktop and mobile, media queries for navbar, hambuger icon

--- src/App.js
+++ src/App.js
@@ -1,28 +1,10 @@
-import './App.css';
+import React from 'react';
+import Navbar from './components/Navbar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div>
+    <Navbar />
+  </div>
+);
 
 export default App;
